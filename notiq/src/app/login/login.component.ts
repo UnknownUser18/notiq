12e73cdeea451fbe10ac6c0918838f4343fe3175
UnknownUser18 <117106948+UnknownUser18@users.noreply.github.com>
2025-06-30 14:19:49 +0200

    Merge pull request #3
    * Enhance flex mixin to support gap parameter and update usages in logi…
    
    * Added user rejection after set amount of login tries

--- notiq/src/app/login/login.component.ts
+++ notiq/src/app/login/login.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, OnDestroy, PLATFORM_ID, ViewChild } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TranslatePipe } from "@ngx-translate/core";
-import { Subject, takeUntil } from 'rxjs';
+import { finalize, Subject, takeUntil } from 'rxjs';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { faCheck, faEye, faEyeSlash, faWarning } from '@fortawesome/free-solid-svg-icons';
+import { animate } from 'animejs';
+import { isPlatformBrowser } from '@angular/common';
+
 @Component({
   selector : 'app-login',
   imports : [
@@ -15,10 +18,16 @@ import { faCheck, faEye, faEyeSlash, faWarning } from '@fortawesome/free-solid-s
   templateUrl : './login.component.html',
   styleUrl : './login.component.scss'
 })
-export class LoginComponent implements OnDestroy {
+export class LoginComponent implements OnDestroy, AfterViewInit {
   private destroy$ : Subject<void> = new Subject<void>();
 
+  protected readonly faEyeSlash = faEyeSlash;
+  protected readonly faEye = faEye;
+  protected readonly faWarning = faWarning;
+  protected readonly faCheck = faCheck;
 
+  protected numberOfAttempts = 0;
+  protected maxAttempts = 3;
   protected showPassword = false;
   protected statusNumber : number | undefined;
   protected isLoggingIn = false;
@@ -29,27 +38,57 @@ export class LoginComponent implements OnDestroy {
     rememberMe : new FormControl(false)
   });
 
+  @ViewChild('main') main! : ElementRef;
 
-  constructor(private authService : AuthService) {
-  }
-
-  protected login() : void {
-    this.loginForm.markAllAsTouched();
-    if (this.loginForm.invalid) return;
-    this.isLoggingIn = true;
-    const { username, password, rememberMe } = this.loginForm.value;
-    this.authService.login(username!, password!, rememberMe!).pipe(takeUntil(this.destroy$)).subscribe({
+  constructor(@Inject(PLATFORM_ID) private platformId : Object, private authService : AuthService) {
+    this.authService.checkLoginStatus().pipe(takeUntil(this.destroy$)).subscribe({
       next : (response) => {
-        this.isLoggingIn = false;
-        this.statusNumber = response.status;
+        if (response.status === 200)
+          this.loginForm.enable();
       },
       error : (error) => {
-        this.isLoggingIn = false;
-        this.statusNumber = error.status;
+        if (error.status === 403)
+          this.loginForm.disable();
       }
     });
   }
 
+  protected login() : void {
+    this.loginForm.markAllAsTouched();
+    if (this.loginForm.invalid || this.isLoggingIn) return;
+    this.isLoggingIn = true;
+    const { username, password, rememberMe } = this.loginForm.value;
+    this.authService.login(username!, password!, rememberMe!).pipe(
+      takeUntil(this.destroy$),
+      finalize(() => {
+        this.isLoggingIn = false;
+        this.numberOfAttempts++;
+        if (this.numberOfAttempts < this.maxAttempts) return;
+        this.loginForm.disable();
+        this.authService.rejectLogin().pipe(takeUntil(this.destroy$)).subscribe({
+          error : () => {
+          }
+        });
+      })).subscribe({
+      next : (response) => {
+        this.statusNumber = response.status;
+      },
+      error : (error) => {
+        this.statusNumber = error.status;
+      },
+    });
+  }
+
+  public ngAfterViewInit() : void {
+    if (!isPlatformBrowser(this.platformId)) return;
+    animate(this.main.nativeElement!, {
+      y : [100, 0],
+      opacity : [0, 1],
+      duration : 750,
+      delay : 50,
+      easing : 'easeInOutQuad'
+    });
+  }
 
   public ngOnDestroy() : void {
     this.destroy$.next();
@@ -63,9 +102,4 @@ export class LoginComponent implements OnDestroy {
     this.loginForm.markAsUntouched();
     this.loginForm.updateValueAndValidity();
   }
-
-  protected readonly faEyeSlash = faEyeSlash;
-  protected readonly faEye = faEye;
-  protected readonly faWarning = faWarning;
-  protected readonly faCheck = faCheck;
 }
