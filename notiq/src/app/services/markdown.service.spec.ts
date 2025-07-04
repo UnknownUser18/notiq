59a411e85fa2671743b37e7df69839b7f570baf0
UnknownUser18 <unknownuser75@proton.me>
2025-07-04 15:49:07 +0200

    Added functionality for lists in MarkdownService Written in markdown.service.spec.ts tests
    * Amend
    Fixed functionality with mixed content

--- notiq/src/app/services/markdown.service.spec.ts
+++ notiq/src/app/services/markdown.service.spec.ts
@@ -34,6 +34,36 @@ describe('MarkdownService', () => {
 >> ... you know, just to be sure
 > And another line
 >>> and another nested blockquote.`,
+    `- This is an unordered list item`,
+    `1. This is an ordered list item`,
+    `- This is an unordered list item with a line break
+- Another item in the same list`,
+    `1. This is an ordered list item with a line break,
+1. Another item in the same list`,
+    `- This is an test of an unordered list,
+- That has some line breaks
+ + And a nested list`,
+    `1. This is an test of an ordered list,
+1. That has some line breaks
+  1. And a nested list`,
+
+    `+ This is an complicated list,
+ + Yap Yap yap
+- And a nested list
+ 1. With a nested ordered list
+ 2. and a dih
+- And a nested ordered list`,
+
+    `1. This is an complicated list,
+1. Yap Yap yap
+1. And a nested list
+ 1. With a nested ordered list
+ 1. And a dih`,
+    `Some text...
++ This is an unordered list item
++ Another item in the same list
+Blah blah blah
+> This is a blockquote`,
   ]
 
   it('should convert h1 markdown to HTML', () => {
@@ -108,6 +138,76 @@ describe('MarkdownService', () => {
       '<p>... you know, just to be sure</p></blockquote>',
       '<p>And another line</p>',
       '<blockquote><blockquote><p>and another nested blockquote.</p></blockquote></blockquote></blockquote>'
+    ]);
+  });
+
+  it('should convert unordered list item to HTML', () => {
+    const html = service.convertToHTML(markdown[13]);
+    expect(html).toEqual(['<ul><li>This is an unordered list item</li></ul>']);
+  });
+
+  it('should convert ordered list item to HTML', () => {
+    const html = service.convertToHTML(markdown[14]);
+    expect(html).toEqual(['<ol><li>This is an ordered list item</li></ol>']);
+  });
+
+  it('should convert unordered list with line break to HTML', () => {
+    const html = service.convertToHTML(markdown[15]);
+    expect(html).toEqual([
+      '<ul><li>This is an unordered list item with a line break</li>',
+      '<li>Another item in the same list</li></ul>'
+    ]);
+  });
+
+  it('should convert ordered list with line break to HTML', () => {
+    const html = service.convertToHTML(markdown[16]);
+    expect(html).toEqual([
+      '<ol><li>This is an ordered list item with a line break,</li>',
+      '<li>Another item in the same list</li></ol>'
+    ]);
+  });
+
+  it('should convert unordered list with nested list to HTML', () => {
+    const html = service.convertToHTML(markdown[17]);
+    expect(html).toEqual([
+      '<ul><li>This is an test of an unordered list,</li>',
+      '<li>That has some line breaks',
+      '<ul><li>And a nested list</li></ul></li></ul>'
+    ]);
+  });
+
+  it('should convert ordered list with nested list to HTML', () => {
+    const html = service.convertToHTML(markdown[19]);
+    expect(html).toEqual([
+      '<ul><li>This is an complicated list,',
+      '<ul><li>Yap Yap yap</li></ul>',
+      '<li>And a nested list',
+      '<ol><li>With a nested ordered list</li>',
+      '<li>and a dih</li></ol></li>',
+      '<li>And a nested ordered list</li></ul>',
+    ]);
+  });
+
+  it('should convert ordered list with nested list to HTML', () => {
+    const html = service.convertToHTML(markdown[20]);
+    expect(html).toEqual([
+      '<ol><li>This is an complicated list,</li>',
+      '<li>Yap Yap yap</li>',
+      '<li>And a nested list',
+      '<ol><li>With a nested ordered list</li>',
+      '<li>And a dih</li></ol></li></ol>'
+    ]);
+  });
+
+  it('should handle mixed content', () => {
+    const html = service.convertToHTML(markdown[21]);
+    console.log(html)
+    expect(html).toEqual([
+      '<p>Some text...</p>',
+      '<ul><li>This is an unordered list item</li>',
+      '<li>Another item in the same list</li></ul>',
+      '<p>Blah blah blah</p>',
+      '<blockquote><p>This is a blockquote</p></blockquote>'
     ]);
   })
 });
